7d02c0d736ff1184b7d68158431db7e9aa3b434c
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2014-06-10 17:21:50 +0800

    update scripts

--- scripts/mod_his.sh
+++ scripts/mod_his.sh
@@ -17,12 +17,12 @@ then
     an="hongxun.lhx"
     am="hongxun.lhx@alibaba-inc.com"
 fi
-
 if [ "$GIT_COMMITTER_EMAIL" = "itaofe@gmail.com" ]
 then
     cn="半边"
     cm="zangtao.zt@alibaba-inc.com"
 fi
+
 if [ "$GIT_AUTHOR_EMAIL" = "itaofe@gmail.com" ]
 then
     an="半边"
@@ -38,6 +38,18 @@ then
     an="何道"
     am="shuangling.ysl@alibaba-inc.com"
 fi
+
+if [ "$GIT_COMMITTER_EMAIL" = "xufayflying@gmail.com" ]
+then
+    cn="何道"
+    cm="shuangling.ysl@alibaba-inc.com"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "xufayflying@gmail.com" ]
+then
+    an="何道"
+    am="shuangling.ysl@alibaba-inc.com"
+fi
+
 
 export GIT_AUTHOR_NAME="$an"
 export GIT_AUTHOR_EMAIL="$am"
